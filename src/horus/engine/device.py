67b7525676ddfa2336c266d1691cd0221b079473
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2014-03-04 18:24:12 +0100

    File Headers corrected.

--- src/horus/engine/device.py
+++ src/horus/engine/device.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #-----------------------------------------------------------------------#
-#																		#
-# This file is part of the Horus Project								#
-#																		#
-# Copyright (C) 2014 Mundo Reader S.L. 									#
-# 																		#
-# Date: March 2014														#
-# Author: Jesús Arroyo Torrens <jesus.arroyo@bq.com>					#
-#																		#
-# This program is free software: you can redistribute it and/or modify	#
-# it under the terms of the GNU General Public License as published by	#
-# the Free Software Foundation, either version 3 of the License, or 	#
-# (at your option) any later version. 									#
-#																		#
-# This program is distributed in the hope that it will be useful,		#
-# but WITHOUT ANY WARRANTY; without even the implied warranty of		#
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 			#
-# GNU General Public License for more details. 							#
-#																		#
-# You should have received a copy of the GNU General Public License 	#
-# along with this program. If not, see <http://www.gnu.org/licenses/>. 	#
-#																		#
+#                                                                       #
+# This file is part of the Horus Project                                #
+#                                                                       #
+# Copyright (C) 2014 Mundo Reader S.L.                                  #
+#                                                                       #
+# Date: March 2014                                                      #
+# Author: Jesús Arroyo Torrens <jesus.arroyo@bq.com>                    #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.  #
+#                                                                       #
 #-----------------------------------------------------------------------#
 
 __author__ = "Jesús Arroyo Torrens <jesus.arroyo@bq.com>"
