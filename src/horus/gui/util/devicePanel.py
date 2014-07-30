aae51e6f105f1df88871cefea01ec3ed2d46ca7b
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2014-07-30 16:36:13 +0200

    [HORUS-39] Step degrees/delay added to control workbench.

--- src/horus/gui/util/devicePanel.py
+++ src/horus/gui/util/devicePanel.py
@@ -39,11 +39,26 @@ class DevicePanel(wx.lib.scrolledpanel.ScrolledPanel):
         """"""
         wx.lib.scrolledpanel.ScrolledPanel.__init__(self, parent=parent, size=(270, 0))
 
+        self.scanner = self.GetParent().GetParent().GetParent().scanner
+
         self.SetupScrolling()
 
         #-- Graphic elements
         cameraParamsStaticText = wx.StaticText(self, wx.ID_ANY, _("Device Parameters"), style=wx.ALIGN_CENTRE)
         cameraParamsStaticText.SetFont((wx.Font(wx.SystemSettings.GetFont(wx.SYS_ANSI_VAR_FONT).GetPointSize(), wx.FONTFAMILY_DEFAULT, wx.NORMAL, wx.FONTWEIGHT_BOLD)))
+
+        stepDegreesLabel = wx.StaticText(self, label=_(u"Step degrees (º) :"))
+        self.stepDegreesText = wx.TextCtrl(self, value=profile.getProfileSetting('step_degrees'), size=(82,-1))
+        stepDelayLabel = wx.StaticText(self, label=_("Step delay (us) :"))
+        self.stepDelayText = wx.TextCtrl(self, value=profile.getProfileSetting('step_delay'), size=(92,-1))
+
+        applyConfigButton = wx.Button(self, -1, _("Apply Configuration"), size=(215,-1))
+
+        #applyConfigButton = wx.Button(self, -1, _("Apply Configuration"))
+
+        #-- Event
+
+        applyConfigButton.Bind(wx.EVT_BUTTON, self.onApplyConfiguration)
 
         #-- Layout
         vbox = wx.BoxSizer(wx.VERTICAL)
@@ -51,9 +66,38 @@ class DevicePanel(wx.lib.scrolledpanel.ScrolledPanel):
         vbox.Add(cameraParamsStaticText, 0, wx.ALL, 10)
         vbox.Add(wx.StaticLine(self), 0, wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, 5)
 
+        hbox = wx.BoxSizer(wx.HORIZONTAL)   
+        hbox.Add(stepDegreesLabel, 0, wx.ALL^wx.BOTTOM, 18)
+        hbox.Add(self.stepDegreesText, 0, wx.ALL^wx.LEFT, 12)
+        vbox.Add(hbox)
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)   
+        hbox.Add(stepDelayLabel, 0, wx.ALL^wx.BOTTOM, 18)
+        hbox.Add(self.stepDelayText, 0, wx.ALL^wx.LEFT, 12)
+        vbox.Add(hbox)
+
+        vbox.Add((0, 0), 1, wx.EXPAND)
+
+        vbox.Add(applyConfigButton, 0, wx.ALL, 18)
+
         self.SetSizer(vbox)
 
-        self.Centre()
+    def onApplyConfiguration(self, event):
+
+        if self.stepDegreesText.GetValue() is not None:
+            profile.putProfileSetting('step_degrees', float((self.stepDegreesText.GetValue()).replace(',','.')))
+
+        if self.stepDelayText.GetValue() is not None:
+            profile.putProfileSetting('step_delay', int(self.stepDelayText.GetValue()))
+
+        if self.scanner.isConnected:
+            self.scanner.device.sendConfiguration(profile.getProfileSettingFloat('step_degrees'), profile.getProfileSettingInteger('step_delay'))
 
     def updateProfileToAllControls(self):
-        pass
+        degrees = profile.getProfileSettingFloat('step_degrees')
+        self.stepDegreesText.SetValue(str(degrees))
+        delay = profile.getProfileSettingInteger('step_delay')
+        self.stepDelayText.SetValue(str(delay))
+
+        if self.scanner.isConnected:
+            self.scanner.device.sendConfiguration(degrees, delay)
