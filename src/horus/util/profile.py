10c3ac03b627d218565961b0ea8aec2a16bc1a20
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2014-09-04 11:30:33 +0200

    [HORUS-50] Calibration default values modified.

--- src/horus/util/profile.py
+++ src/horus/util/profile.py
@@ -244,15 +244,15 @@ setting('machine_height', '200', float, 'machine', 'hidden').setLabel(_("Maximum
 setting('machine_center_is_zero', 'True', bool, 'machine', 'hidden').setLabel(_("Machine center 0,0"), _("Machines firmware defines the center of the bed as 0,0 instead of the front left corner."))
 setting('machine_shape', 'Circular', ['Square','Circular'], 'machine', 'hidden').setLabel(_("Build area shape"), _("The shape of machine build area."))
 
-setting('camera_matrix', ([[1.4177e+03,0,4.7734e+02],[0,1.4126e+03,6.5224e+02],[0,0,1]]), numpy.ndarray, 'advanced', _('Calibration Matrix'))
-setting('distortion_vector',([0.0,0.0,0.0,0.0,0.0]),numpy.ndarray,'advanced',_('Distortion Vector'))
+setting('camera_matrix', ([[1410,0,480],[0,1410,640],[0,0,1]]), numpy.ndarray, 'advanced', _('Calibration Matrix'))
+setting('distortion_vector',([0,0,0,0,0]),numpy.ndarray,'advanced',_('Distortion Vector'))
 setting('use_distortion', False, bool, 'advanced', _('Use Distortion'))
 
-setting('laser_coordinates', ([[300,300],[300,300]]), numpy.ndarray, 'advanced', _('Laser Coordinates'))
-setting('laser_depth', 300.0, float, 'advanced', _('Laser Depth'))
+setting('laser_coordinates', ([[480,480],[480,480]]), numpy.ndarray, 'advanced', _('Laser Coordinates'))
+setting('laser_depth', 315.0, float, 'advanced', _('Laser Depth'))
 
 setting('rotation_matrix', ([[0,-1,0],[0,0,-1],[-1,0,0]]), numpy.ndarray, 'advanced', _('Rotation Matrix'))
-setting('translation_vector', ([0,80,320]), numpy.ndarray, 'advanced', _('Translation Matrix'))
+setting('translation_vector', ([0.0,90.0,315.0]), numpy.ndarray, 'advanced', _('Translation Matrix'))
 		
 setting('pattern_rows', 11, int, 'advanced', _('Pattern Rows'))
 setting('pattern_columns', 6, int, 'advanced', _('Pattern Columns'))
