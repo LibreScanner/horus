d05c67407b5c69ada6b221cd10538e4ff8e44b19
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2014-11-12 10:28:57 +0100

    Licese modified to GPL v2.

--- src/horus/gui/workbench/scanning/panels.py
+++ src/horus/gui/workbench/scanning/panels.py
@@ -11,7 +11,7 @@
 #                                                                       #
 # This program is free software: you can redistribute it and/or modify  #
 # it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
+# the Free Software Foundation, either version 2 of the License, or     #
 # (at your option) any later version.                                   #
 #                                                                       #
 # This program is distributed in the hope that it will be useful,       #
@@ -25,7 +25,7 @@
 #-----------------------------------------------------------------------#
 
 __author__ = "Jesús Arroyo Torrens <jesus.arroyo@bq.com>"
-__license__ = "GNU General Public License v3 http://www.gnu.org/licenses/gpl.html"
+__license__ = "GNU General Public License v2 http://www.gnu.org/licenses/gpl.html"
 
 
 import wx._core
