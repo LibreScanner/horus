d5c7d0e785642d7780ef28e28ffe060710eb2a7e
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2015-03-04 11:33:27 +0100

    [HORUS-210] Pattern Distance Window improved. New image added

--- src/horus/gui/util/patternDistanceWindow.py
+++ src/horus/gui/util/patternDistanceWindow.py
@@ -6,7 +6,7 @@
 #                                                                       #
 # Copyright (C) 2014-2015 Mundo Reader S.L.                             #
 #                                                                       #
-# Date: October 2014                                                    #
+# Date: February 2015                                                   #
 # Author: Irene Sanz Nieto <irene.sanz@bq.com>                          #
 #                                                                       #
 # This program is free software: you can redistribute it and/or modify  #
@@ -36,7 +36,7 @@ from horus.engine import calibration
 class PatternDistanceWindow(wx.Dialog):
 
     def __init__(self, parent):
-        super(PatternDistanceWindow, self).__init__(parent, size=(300,350), style=wx.DEFAULT_FRAME_STYLE^wx.RESIZE_BORDER)
+        super(PatternDistanceWindow, self).__init__(parent, title=_('Pattern distance'), size=(420,-1), style=wx.DEFAULT_FRAME_STYLE^wx.RESIZE_BORDER)
 
         self.parent = parent
         self.cameraIntrinsics = calibration.CameraIntrinsics.Instance()
@@ -45,75 +45,65 @@ class PatternDistanceWindow(wx.Dialog):
         self.platformExtrinsics = calibration.PlatformExtrinsics.Instance()
 
         #-- Elements
-        self.text = wx.StaticText(self, label=_('Pattern distance value must be a number higher than 0. Please, change it in the textbox below.'))
-        self.text.Wrap(280)
-        # self.pattern_image = wx.Image(resources.getPathForImage("pattern_distance.jpg"))
-        img = wx.Image(resources.getPathForImage("pattern_distance.jpg"), wx.BITMAP_TYPE_ANY)
-        img = img.Scale(280, 160, wx.IMAGE_QUALITY_HIGH)
-        self.pattern_image=wx.StaticBitmap(self, wx.ID_ANY, wx.BitmapFromImage(img))
-
-
-        self.pattern_text = wx.StaticText(self, label=_('Pattern distance (mm)'))
-        self.pattern_text.SetToolTip(wx.ToolTip(_('Distance between the upper edge of the chess row closer to the platform and the platform.')))
-
-        self.pattern_textbox = wx.TextCtrl(self, value = str(profile.getProfileSettingFloat('pattern_distance')))
-        self.okButton = wx.Button(self, label=_("OK"))
-        self.text.SetToolTip(wx.ToolTip(_('Distance between the upper edge of the chess row closer to the platform and the platform.')))
+        self.description = wx.StaticText(self, label=_('Pattern distance value must be a number higher than 0. Please, change it in the textbox below.'))
+        self.description.Wrap(400)
+        tooltip = _('Distance between the upper edge of the chess row closer to the platform and the platform.')
+        self.image = wx.Image(resources.getPathForImage("pattern_distance.jpg"), wx.BITMAP_TYPE_ANY)
+        self.patternImage = wx.StaticBitmap(self, wx.ID_ANY, wx.BitmapFromImage(self.image))
+        self.patternImage.SetToolTip(wx.ToolTip(tooltip))
+        self.patternLabel = wx.StaticText(self, label=_('Pattern distance (mm)'))
+        self.patternLabel.SetToolTip(wx.ToolTip(tooltip))
+        self.patternTextbox = wx.TextCtrl(self, value = str(profile.getProfileSettingFloat('pattern_distance')))
+        self.okButton = wx.Button(self, label=_('OK'))
+        self.okButton.Disable()
+        self.cancelButton = wx.Button(self, label=_('Cancel'))
+        
+        #-- Events
+        self.patternTextbox.Bind(wx.EVT_TEXT, self.onTextBoxChanged)
+        self.cancelButton.Bind(wx.EVT_BUTTON, self.onCancel)
+        self.okButton.Bind(wx.EVT_BUTTON, self.onClose)
+        self.Bind(wx.EVT_CLOSE, self.onClose)
 
         #-- Layout
         vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add(self.description, 0, wx.ALL|wx.CENTER, 10)
+        vbox.Add(self.patternImage, 0, wx.ALL|wx.CENTER, 10)
         hbox = wx.BoxSizer(wx.HORIZONTAL)
-        hbox.Add(self.text, 0, wx.ALL|wx.EXPAND, 12)
-        vbox.Add(hbox, 0, wx.ALL|wx.EXPAND,-1)
-        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        hbox2.Add(self.pattern_image, 0, wx.ALL, 10)
-        vbox.Add(hbox2, 0, wx.ALL, -1)
-        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-        hbox1.Add(self.pattern_text, 0, wx.ALL|wx.EXPAND, 12)
-        hbox1.Add(self.pattern_textbox, 0, wx.ALL|wx.EXPAND, 12)
-        vbox.Add(hbox1, 0, wx.ALL|wx.EXPAND,-1)
-        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-        hbox3.Add(self.okButton, 0, wx.ALL|wx.EXPAND, 12)
-        vbox.Add(hbox3, 0, wx.ALIGN_CENTER_HORIZONTAL,-1)
+        hbox.Add(self.patternLabel, 0, wx.ALL, 7)
+        hbox.Add(self.patternTextbox, 0, wx.ALL, 3)
+        hbox.Add(self.okButton, 0, wx.ALL, 3)
+        hbox.Add(self.cancelButton, 0, wx.ALL, 3)
+        vbox.Add(hbox, 0, wx.ALL|wx.CENTER, 10)
         self.SetSizer(vbox)
+        self.Center()
+        self.Fit()
 
-        self.pattern_textbox.Bind(wx.EVT_TEXT, self.onTextBoxChanged)
-        self.Bind(wx.EVT_CLOSE, self.onClose)
-        self.okButton.Bind(wx.EVT_BUTTON, self.onClose)
-        self.okButton.Disable()
-
-        self.Centre()
         self.ShowModal()
 
     def onTextBoxChanged(self, event):
         try:
-            value=float(self.pattern_textbox.GetValue())
+            value = float(self.patternTextbox.GetValue())
             if value > 0:
-                profile.putProfileSetting('pattern_distance',self.pattern_textbox.GetValue())
-
-                self.cameraIntrinsics.setPatternParameters(profile.getProfileSettingInteger('pattern_rows'),
-                                                           profile.getProfileSettingInteger('pattern_columns'),
-                                                           profile.getProfileSettingInteger('square_width'),
-                                                           profile.getProfileSettingFloat('pattern_distance'))
-
-                self.simpleLaserTriangulation.setPatternParameters(profile.getProfileSettingInteger('pattern_rows'),
-                                                                   profile.getProfileSettingInteger('pattern_columns'),
-                                                                   profile.getProfileSettingInteger('square_width'),
-                                                                   profile.getProfileSettingFloat('pattern_distance'))
-
-                self.laserTriangulation.setPatternParameters(profile.getProfileSettingInteger('pattern_rows'),
-                                                             profile.getProfileSettingInteger('pattern_columns'),
-                                                             profile.getProfileSettingInteger('square_width'),
-                                                             profile.getProfileSettingFloat('pattern_distance'))
-
-                self.platformExtrinsics.setPatternParameters(profile.getProfileSettingInteger('pattern_rows'),
-                                                             profile.getProfileSettingInteger('pattern_columns'),
-                                                             profile.getProfileSettingInteger('square_width'),
-                                                             profile.getProfileSettingFloat('pattern_distance'))
+                self.setPatternDistance(value)
                 self.okButton.Enable()
-
         except:
             self.okButton.Disable()
+
+    def setPatternDistance(self, patternDistance):
+        profile.putProfileSetting('pattern_distance', patternDistance)
+
+        patternRows = profile.getProfileSettingInteger('pattern_rows')
+        patternColumns = profile.getProfileSettingInteger('pattern_columns')
+        squareWidth = profile.getProfileSettingInteger('pattern_columns')
+
+        self.cameraIntrinsics.setPatternParameters(patternRows, patternColumns, squareWidth, patternDistance)
+        self.simpleLaserTriangulation.setPatternParameters(patternRows, patternColumns, squareWidth, patternDistance)
+        self.laserTriangulation.setPatternParameters(patternRows, patternColumns, squareWidth, patternDistance)
+        self.platformExtrinsics.setPatternParameters(patternRows, patternColumns, squareWidth, patternDistance)
+
+    def onCancel(self, event):
+        self.setPatternDistance(0)
+        self.Destroy()
 
     def onClose(self, event):
         self.Destroy()
