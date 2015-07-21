794357c9a555b30352ab1ede27e986f8f037d6f9
Miro Hrončok <miro@hroncok.cz>
2015-07-21 14:50:43 +0200

    Install correctly with setup.py
    This change makes the package correctly installable on Linux
    without any hacks and Bash scripts.

--- setup.py
+++ setup.py
@@ -6,30 +6,35 @@ from setuptools import setup, find_packages
 
 version = os.environ.get('VERSION')
 
-def package_data_dirs(source, sub_folders):
-	dirs = []
 
-	for d in sub_folders:
-		for dirname, _, files in os.walk(os.path.join(source, d)):
-			dirname = os.path.relpath(dirname, source)
-			for f in files:
-				dirs.append(os.path.join(dirname, f))
-	return dirs
+def package_data_dirs(source, basedir='/usr/share/horus'):
+    dirs = []
 
-setup(name='Horus',
-      version=version,
-      author='Jesús Arroyo Torrens',
-      author_email='jesus.arroyo@bq.com',
-      description='Horus is a full software solution for 3D scanning',
+    for dirname, _, files in os.walk(source):
+        dirname = os.path.relpath(dirname, source)
+        for f in files:
+            dirs.append((os.path.join(basedir, dirname),
+                         [os.path.join(source, dirname, f)]))
 
-      license='GPLv2',
-      keywords = "horus ciclop scanning 3d",
-      url='https://www.diwo.bq.com',
+    dirs.append(('/usr/share/applications', ['pkg/linux/horus.desktop']))
 
-      packages = ['horus'],
-      package_dir = {'horus': '.'},
-      package_data = {'horus': package_data_dirs('.', ['doc', 'res', 'src'])},
-      
-      scripts=['pkg/linux/horus'],
-      data_files=[('/usr/share/applications', ['pkg/linux/horus.desktop'])]
-     )
+    return dirs
+
+
+setup(
+    name='Horus',
+    version=version,
+    author='Jesús Arroyo Torrens',
+    author_email='jesus.arroyo@bq.com',
+    description='Horus is a full software solution for 3D scanning',
+
+    license='GPLv2',
+    keywords="horus ciclop scanning 3d",
+    url='https://www.diwo.bq.com',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+
+    scripts=['horus'],
+    data_files=package_data_dirs('res'),
+)
